2571ea0b85be22ca3bc4e008b2cee333f08d0aae
MikkoAyaka <3401286177@qq.com>
2023-04-16 08:36:15 +0800

    引入HeadBar到MainActivity

--- app/src/main/java/org/ccaa/android/ccaa_smp/MainActivity.kt
+++ app/src/main/java/org/ccaa/android/ccaa_smp/MainActivity.kt
@@ -24,11 +24,8 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
-
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
 
         binding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
